5d8213c99a84917ae20c2bb5e8ab08e3b49c2ef1
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-05-15 16:37:56 +0900

    Add Queue

---
+++ queue/queue.go
@@ -0,0 +1,38 @@
+package queue
+
+import (
+	"container/list"
+	"fmt"
+)
+
+//Queue ...
+type Queue struct {
+	list *list.List
+}
+
+//New new Queue()
+func New() *Queue {
+	return &Queue{list: &list.List{}}
+}
+
+//Push add item
+func (s *Queue) Push(val int) {
+	s.list.PushBack(val)
+}
+
+// Pop get and remove last item
+func (s *Queue) Pop() {
+	if s.Empty() {
+		return
+	}
+	e := s.list.Front()
+	fmt.Println(s.list.Remove(e))
+}
+
+//Empty ...
+func (s *Queue) Empty() bool {
+	if s.list.Len() == 0 {
+		return true
+	}
+	return false
+}
